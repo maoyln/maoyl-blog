da7c9bc3a55035ef02318638828b674d7c1499ce
maoyl <maoyl@glodon.com>
2021-06-07 20:01:34 +0800

    百度收录推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,5 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://maoyln.vercel.app&token=oK6FHYEhsLFKoBYS"
-
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://maoyln.vercel.app&token=l6eeyUi4FhZrDXlf"
 rm -rf urls.txt # 灭迹
