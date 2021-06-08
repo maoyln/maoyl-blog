4e9be22f33d46a5568242daf19f629199882baa5
maoyl <maoyl@glodon.com>
2021-06-08 11:24:16 +0800

    网站备案号

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/maoyl-logo.png', // 导航栏logo
-  repo: 'maoyln/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+  repo: 'maoyln/maoyl-bolg', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -81,7 +81,8 @@ module.exports = {
     // 页脚信息
     createYear: 2019, // 博客创建年份
     copyrightInfo:
-      'maoyln | <a href="https://github.com/maoyln/maoyl-bolg/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      `备案号：<a href="https://beian.miit.gov.cn/" target="_blank">京ICP备19058102号-1</a>` // 博客版权信息，支持a标签
+      // `京ICP备19058102号<a href="https://github.com/maoyln/maoyl-bolg/blob/master/LICENSE" target="_blank">MIT License1</a>'` // 博客版权信息，支持a标签
   },
   htmlModules // 插入hmtl(广告)模块
 }
