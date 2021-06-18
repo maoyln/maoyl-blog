1ad3c190aafff312d650929820a44b6e3417503a
maoyl <maoyl@glodon.com>
2021-06-18 09:06:50 +0800

    解决build错误问题

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -70,10 +70,10 @@ module.exports = [
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
         ],
       },
-      {
-        text: '学习笔记',
-        items: [],
-      },
+      // {
+      //   text: '学习笔记',
+      //   items: [],
+      // },
     ],
   },
   {
