ca8d12994a94fd5051b021e0679c4d1736ffde5d
maoyl <maoyl@glodon.com>
2021-06-19 20:47:16 +0800

    去除广告

--- docs/.vuepress/config/themeConfig/htmlModules.js
+++ docs/.vuepress/config/themeConfig/htmlModules.js
@@ -18,81 +18,81 @@
  * ②注：windowLB 和 windowRB：1.展示区块宽高最大是200*200px。2.请给自定义元素定一个不超过200px的固定宽高。3.在屏宽小于960px时无论如何都不会显示。
  */
 
-module.exports = {
-  homeSidebarB:
-    `<!-- 纵向自适应 -->
-    <ins class="adsbygoogle"
-        style="display:block;padding: 0.95rem;"
-        data-ad-client="ca-pub-7828333725993554"
-        data-ad-slot="7802654582"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>`,
-  // sidebarT:
-  //   `<!--  固定100% * 150px可显示，max-height:150px 未见显示-->
-  //   <ins class="adsbygoogle"
-  //         style="display:inline-block;width:100%;max-height:150px"
-  //         data-ad-client="ca-pub-7828333725993554"
-  //         data-ad-slot="6625304284"></ins>
-  //     <script>
-  //         (adsbygoogle = window.adsbygoogle || []).push({});
-  //     </script>`,
-  sidebarB:
-    `<!-- 正方形 -->
-      <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="3508773082"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`,
-  pageT:
-    `<!-- 固定100% * 90px可显示，max-height:90px未见显示-->
-     <ins class="adsbygoogle"
-          style="display:inline-block;width:100%;max-height:90px"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="6625304284"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`,
-  // pageTshowMode: 'article',
-  pageB:
-    `<!-- 横向自适应 -->
-      <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="6620245489"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`,
-  // pageBshowMode: 'article',
-  // windowLB: // 会遮挡部分侧边栏
-  //   `<!-- 固定200*200px -->
-  //     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  //     <ins class="adsbygoogle"
-  //         style="display:inline-block;width:200px;height:200px"
-  //         data-ad-client="ca-pub-7828333725993554"
-  //         data-ad-slot="6625304284"></ins>
-  //     <script>
-  //         (adsbygoogle = window.adsbygoogle || []).push({});
-  //     </script>`,
-  windowRB:
-     `<!-- 固定160*160px -->
-      <ins class="adsbygoogle"
-          style="display:inline-block;max-width:160px;max-height:160px"
-          data-ad-client="ca-pub-7828333725993554"
-          data-ad-slot="8377369658"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-      `,
-}
+// module.exports = {
+//   homeSidebarB:
+//     `<!-- 纵向自适应 -->
+//     <ins class="adsbygoogle"
+//         style="display:block;padding: 0.95rem;"
+//         data-ad-client="ca-pub-7828333725993554"
+//         data-ad-slot="7802654582"
+//         data-ad-format="auto"
+//         data-full-width-responsive="true"></ins>
+//     <script>
+//         (adsbygoogle = window.adsbygoogle || []).push({});
+//     </script>`,
+//   // sidebarT:
+//   //   `<!--  固定100% * 150px可显示，max-height:150px 未见显示-->
+//   //   <ins class="adsbygoogle"
+//   //         style="display:inline-block;width:100%;max-height:150px"
+//   //         data-ad-client="ca-pub-7828333725993554"
+//   //         data-ad-slot="6625304284"></ins>
+//   //     <script>
+//   //         (adsbygoogle = window.adsbygoogle || []).push({});
+//   //     </script>`,
+//   sidebarB:
+//     `<!-- 正方形 -->
+//       <ins class="adsbygoogle"
+//           style="display:block"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="3508773082"
+//           data-ad-format="auto"
+//           data-full-width-responsive="true"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>`,
+//   pageT:
+//     `<!-- 固定100% * 90px可显示，max-height:90px未见显示-->
+//      <ins class="adsbygoogle"
+//           style="display:inline-block;width:100%;max-height:90px"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="6625304284"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>`,
+//   // pageTshowMode: 'article',
+//   pageB:
+//     `<!-- 横向自适应 -->
+//       <ins class="adsbygoogle"
+//           style="display:block"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="6620245489"
+//           data-ad-format="auto"
+//           data-full-width-responsive="true"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>`,
+//   // pageBshowMode: 'article',
+//   // windowLB: // 会遮挡部分侧边栏
+//   //   `<!-- 固定200*200px -->
+//   //     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+//   //     <ins class="adsbygoogle"
+//   //         style="display:inline-block;width:200px;height:200px"
+//   //         data-ad-client="ca-pub-7828333725993554"
+//   //         data-ad-slot="6625304284"></ins>
+//   //     <script>
+//   //         (adsbygoogle = window.adsbygoogle || []).push({});
+//   //     </script>`,
+//   windowRB:
+//      `<!-- 固定160*160px -->
+//       <ins class="adsbygoogle"
+//           style="display:inline-block;max-width:160px;max-height:160px"
+//           data-ad-client="ca-pub-7828333725993554"
+//           data-ad-slot="8377369658"></ins>
+//       <script>
+//           (adsbygoogle = window.adsbygoogle || []).push({});
+//       </script>
+//       `,
+// }
 
 
 // module.exports = {
