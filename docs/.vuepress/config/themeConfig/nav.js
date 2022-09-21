e5ec9d22c78d048d2c6738d7a6262646a44a5fc7
maoyl <maoyl@glodon.com>
2022-09-21 18:49:53 +0800

    WEB网页性能标准

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -86,6 +86,8 @@ module.exports = [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
       { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
       { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      { text: '网页性能', link: '/pages/21e023/' },
+      
       {
         text: '学习笔记', 
         items: [
