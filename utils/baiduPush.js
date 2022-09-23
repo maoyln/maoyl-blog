034876198a72e63b5572fb19c90b22cdfc6e3a41
maoyl <maoyl@glodon.com>
2022-09-23 11:25:40 +0800

    托管平台替换

--- utils/baiduPush.js
+++ utils/baiduPush.js
@@ -12,7 +12,7 @@ const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
 if (!DOMAIN) {
   console.log(
     chalk.red(
-      '请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://app.netlify.com/'
+      '请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://maoyl.netlify.com/'
     )
   )
   return
